425dd5c427b537559419b6dea45f2cf93367f29c
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-08 14:42:09 +1000

    Create a new class MovieQuote. Print the favourite quote.

---
+++ Prac_1_Task_5/src/MovieQuote.java
@@ -0,0 +1,9 @@
+public class MovieQuote {
+
+    public static void main (String[] args)
+        {
+            System.out.println("Quote - “Houston, we have a problem.” “We've never lost an American in space, we're sure as hell not gonna lose one on my watch! Failure is not an option.”");
+
+        }
+    }
+

f969a6fc3d7896b03ccdbdfd7fe211de5d82bba9
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-14 19:15:08 +1000

    To print four lines of comments.

---
+++ Prac_1_Task_5/src/Comments.java
@@ -0,0 +1,9 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        System.out.println("\"Program comments are non-executing statements you add to a file for the purpose of documentation.\"");
+        System.out.println("\"Also include the same statement in three different " +
+                "comments in the class; each comment should use one of three different methods of including comments in a Java class.\"");
+
+    }
+}

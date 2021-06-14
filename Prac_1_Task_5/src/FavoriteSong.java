9333b3afc52f4c866aa0d4fb4769097756bafe30
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-14 19:14:30 +1000

    To print four lines of from my favorite song.

---
+++ Prac_1_Task_5/src/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args){
+
+        System.out.println("\"Zimbabwae is a man who tried\"");
+        System.out.println("\"To teach his children what was right\"");
+        System.out.println("\"But then there came a time when war\"");
+        System.out.println("\"Split the family from inside\"");
+
+
+    }
+}

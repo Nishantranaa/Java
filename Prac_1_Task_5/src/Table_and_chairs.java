300a95db174cf20b48ec83502f95b1863e722c59
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-14 15:39:01 +1000

    To use for loops to create row and columns to print a chair pattern.

---
+++ Prac_1_Task_5/src/Table_and_chairs.java
@@ -0,0 +1,49 @@
+public class Table_and_chairs {
+
+    public static void main(String[] args) {
+
+        int i, j;
+        for (i = 3; i >= 1; i--) {
+            System.out.println("*");
+
+
+        }
+        for (j = 1; j <= 5; j++) {
+            System.out.print("*");
+        }
+
+
+        int a, c;
+        for (a = 1; a <= 1; a++) {
+            System.out.println("*");
+        }
+
+        System.out.print("*");
+
+        for (c = 1; c <= 4; c++) {
+            System.out.print(" ");
+            //System.out.print("");
+
+        }
+
+        System.out.print("*");
+        System.out.println(" ");
+        System.out.print("*");
+
+        for (c = 1; c <= 4; c++) {
+            System.out.print(" ");
+            //System.out.print("");
+
+        }
+
+        System.out.print("*");
+        System.out.println("");
+
+
+
+        }
+    }
+
+
+
+

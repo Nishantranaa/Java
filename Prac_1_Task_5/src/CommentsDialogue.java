1447600058f996953393a2af8d8f68326cd41766
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-14 20:56:12 +1000

    To add the comment in a dialogue box.

---
+++ Prac_1_Task_5/src/CommentsDialogue.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class CommentsDialogue {
+    public static void  main(String[] args){
+        JOptionPane.showMessageDialog(null, "\"Program comments are non-executing statements you add to a file for the purpose of documentation.\"\n" +
+                "\"Also include the same statement in three different comments in the class; each comment should \n" +
+                " use one of three different methods of including comments in a Java class.\"\n");
+    }
+}

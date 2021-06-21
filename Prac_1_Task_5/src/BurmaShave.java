9f60e295ec514130f77cb2f9d7ceb4fd3b6d4a45
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-21 17:20:41 +1000

    To create a program which will display multiple dialogue boxes.

---
+++ Prac_1_Task_5/src/BurmaShave.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+public class BurmaShave {
+
+    public static void  main(String[] args){
+        JOptionPane.showMessageDialog(null, "Our fortune");
+        JOptionPane.showMessageDialog(null, "Is your");
+        JOptionPane.showMessageDialog(null, "Shaven face");
+        JOptionPane.showMessageDialog(null, "It's our best");
+        JOptionPane.showMessageDialog(null, "Advertising space");
+        JOptionPane.showMessageDialog(null, "Burma-Shave");
+    }
+
+
+    }
+
+
+
+
+
+
+//        /  /
+//     /
+//
+
+
+// TODO: Use the GUI dialogue box and create each sentence of the the BurmaShave rhyme rhythm in a sequence.

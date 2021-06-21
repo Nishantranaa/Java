2c76388acdaf120e65bafff0fc0e5428441a6c33
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-21 14:29:41 +1000

    To create a program which will calculate the gross dozen and number of eggs. A gross is 144 eggs.

--- Prac_2/src/Dozens.java
+++ Prac_2/src/Dozens.java
@@ -9,11 +9,12 @@ public class Dozens {
 
     public static void main (String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
         System.out.print("How may eggs do you have? ");
         int eggs = sc.nextInt();
         int dozen = eggs/12;
-        int extra_eggs;
+
 
 
         //System.out.println(dozen);
@@ -21,15 +22,20 @@ public class Dozens {
         eggs_on_hand = eggs - 12;
 
 
-        if (dozen != 0 )
+        if (dozen != 0 && eggs < 144 )
         {
-            System.out.print("Your number of eggs is 1 dozen and " + eggs_on_hand);
+
+            System.out.print("Your number of eggs is " + dozen + " dozen and " + eggs_on_hand);
         }
 
+        else if (eggs >= 144){
+
+               int gross = eggs/144;
+               System.out.println("Your number of eggs is, " + gross + " gross, " + eggs/12 + " dozen, " + eggs_on_hand);
+
+        }
         else {
-
             System.out.println("You have less than a dozen of eggs");
-
         }
 
 

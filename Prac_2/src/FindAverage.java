f9535958b2898906704197f2d70558fcbdfa78f3
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-06-23 14:55:25 +1000

    To add a for loop which will add the total results and print statement will contain an expression to divide by the total number of results to give the average.

--- Prac_2/src/FindAverage.java
+++ Prac_2/src/FindAverage.java
@@ -4,33 +4,84 @@
 // To calculate the average and print the result.
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import the class to handle errors.
+import java.lang.IndexOutOfBoundsException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner; // Import the scanner class to read text file.
 public class FindAverage {
 
+    static int ConvertString2Int(String str) {
+
+
+        int number = 0;
+        try {
+            number = Integer.parseInt(str);
+            // System.out.println(number); // output = 25
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+
+
+        return number;
+    }
+
     public static void main(String[] args){
-        try{
+        try {
             File myObject = new File("C:\\Users\\nisha\\OneDrive - James Cook University\\SP22-2021\\Programming 3\\WK 2\\chapter-2-exercises-jc138691-master\\resources\\testdata.txt");
             Scanner myReader = new Scanner(myObject);
-            while (myReader.hasNextLine()){
+            List<String> list = new ArrayList<String>();
+            int listSize = 0;
+            while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+
+                list.add(data);
+
             }
-            if (myObject.exists()) {
-                System.out.println("File name: " + myObject.getName());
-                System.out.println("Absolute path: " + myObject.getAbsolutePath());
-                System.out.println("Writeable: " + myObject.canWrite());
-                System.out.println("Readable " + myObject.canRead());
-                System.out.println("File size in bytes " + myObject.length());
-            } else {
-                System.out.println("The file does not exist.");
-            }
+
+
+                System.out.println(list.get(0));
+                int results = 0;
+
+                // add a while loop so each time it call the method the results are added to the
+               // results variable.
+
+               //System.out.println(list.size()-1);
+
+               for (int i = 1; list.size()-1 >= i; i++){
+
+                   results = results + ConvertString2Int(list.get(i));
+
+               }
+
+                System.out.println("The average exam result is: "+ results/3);
+
+
+
+
+
+
+//            if (myObject.exists()) {
+//                System.out.println("File name: " + myObject.getName());
+//                System.out.println("Absolute path: " + myObject.getAbsolutePath());
+//                System.out.println("Writeable: " + myObject.canWrite());
+//                System.out.println("Readable " + myObject.canRead());
+//                System.out.println("File size in bytes " + myObject.length());
+//            } else {
+//                System.out.println("The file does not exist.");
+//            }
+
             myReader.close();
 
         } catch (FileNotFoundException e)
         {
             System.out.println("An error has occurred.");
-            e.printStackTrace();
+            e.printStackTrace();// good for developer testing.
         }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Your have entered an index that does not exist.");
+            e.printStackTrace(); // good for developer testing.
+        }
+
 
 
     }
